8c773f68326fe5f9aafedceffd7734dcef366fdb
mmmmz <lmz980806@gmail.com>
2025-11-20 18:10:19 +0900

    {25.11.20}, {이명진}, {최종본2 다시 되돌리기}

--- Views/StatusPage.xaml.cs
+++ Views/StatusPage.xaml.cs
@@ -124,6 +124,7 @@ namespace DotBotCarClient.Views
             await App.Network.SendAsync(req);
         }
 
+
         // ======================================================
         // STATUS_RES / BATEERY_RES 처리 (UI 업데이트)
         // ======================================================
